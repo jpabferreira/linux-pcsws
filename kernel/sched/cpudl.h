a208a7b6eec59037b01c527ad4ac206f5c307448
João Pedro Ferreira <jpabferreira@gmail.com>
2013-07-02 23:51:29 +0100

    Initial commit

--- kernel/sched/cpudl.h
+++ kernel/sched/cpudl.h
@@ -6,29 +6,25 @@
 #define IDX_INVALID     -1
 
 struct array_item {
-    u64 dl;
-    int cpu;
+	u64 dl;
+	int cpu;
 };
 
 struct cpudl {
-    raw_spinlock_t lock;
-    int size;
-    int cpu_to_idx[NR_CPUS];
-    struct array_item elements[NR_CPUS];
-    cpumask_var_t free_cpus;
+	raw_spinlock_t lock;
+	int size;
+	int cpu_to_idx[NR_CPUS];
+	struct array_item elements[NR_CPUS];
+	cpumask_var_t free_cpus;
 };
 
 
 #ifdef CONFIG_SMP
-int find_idle_cpu_pcsws(struct cpudl *cp);
-int find_latest_cpu_pcsws(struct cpudl *cp);
-int find_random_stealable_cpu_pcsws(struct cpudl *cp, int cpu);
-int find_earliest_stealable_cpu_pcsws(struct cpudl *cp);
+int cpudl_find(struct cpudl *cp, struct task_struct *p,
+	       struct cpumask *later_mask);
 void cpudl_set(struct cpudl *cp, int cpu, u64 dl, int is_valid);
 int cpudl_init(struct cpudl *cp);
 void cpudl_cleanup(struct cpudl *cp);
-inline int cpudl_maximum(struct cpudl *cp);
-
 #else
 #define cpudl_set(cp, cpu, dl) do { } while (0)
 #define cpudl_init() do { } while (0)
